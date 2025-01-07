4d9acadea11742649373d471a64039add64120cf
Miguel Leal <miguel.leal@sovos.com>
2025-01-06 22:07:30 -0300

    Avances varios

--- inicio.py
+++ inicio.py
@@ -1,11 +1,15 @@
 # Online Python - IDE, Editor, Compiler, Interpreter
 
-valor_ex_1 = {"codigo": 10, "nombre": "producto 1", "precio" : 1000, "disponible" : 5, "minimo": 3}
-valor_ex_2 = {"codigo": 20, "nombre": "producto 2", "precio" : 1500, "disponible" : 3, "minimo": 2}
+valor_ex_1 = {"nombre": "producto 1", "precio" : 1000, "disponible" : 5, "minimo": 3}
+valor_ex_2 = {"nombre": "producto 2", "precio" : 1500, "disponible" : 3, "minimo": 2}
 data_ex = {
   1 : valor_ex_1,
   2 : valor_ex_2
 }
+
+cabecero = [
+    "Cod.", "Nombre", "Precio", "Q", "Min"
+]
 
 def menu():
     while True:
@@ -34,22 +38,41 @@ def gestion_de_productos():
     def agregar_producto():
         print("Agregar producto")
         try:
-            codigo = int(input("Inserte valor número del código del producto "))
+            codigo = int(input("Inserte valor numérico del código del producto "))
             nombre = str(input("Inserte nombre del producto "))
-            precio = int(input("Inserte valor número del precio del producto "))
-            disponible = int(input("Inserte valor número de la cantidad disponible del producto "))
-            minimo = int(input("Inserte valor número del monto mínimo a disponer del producto "))
-            valor_add = {"codigo": codigo, "nombre": nombre, "precio": precio, "disponible": disponible, "minimo": minimo}
-            data_ex[len(data_ex)+1] = valor_add
+            precio = int(input("Inserte valor numérico del precio del producto "))
+            disponible = int(input("Inserte valor numérico de la cantidad disponible del producto "))
+            minimo = int(input("Inserte valor numérico del monto mínimo a disponer del producto "))
+            valor_add = {"nombre": nombre, "precio": precio, "disponible": disponible, "minimo": minimo}
+            data_ex[codigo] = valor_add
             print("Producto agregado")
         except:
             print("Ocurrió un error, intenta de nuevo") #todo: test para probar errores
 
     def editar_producto():
         print("Editar producto")
+        try:
+            #numero = int(input("Inserte valor del número del producto "))
+            codigo = int(input("Inserte valor numérico del código del producto "))
+            nombre = str(input("Inserte nombre del producto "))
+            precio = int(input("Inserte valor numérico del precio del producto "))
+            disponible = int(input("Inserte valor numérico de la cantidad disponible del producto "))
+            minimo = int(input("Inserte valor numérico del monto mínimo a disponer del producto "))
+            valor_add = {"nombre": nombre, "precio": precio, "disponible": disponible, "minimo": minimo}
+            data_ex[codigo] = valor_add
+            print("Producto modificado")
+        except:
+            print("Ocurrió un error, intenta de nuevo") #todo: test para probar errores
 
     def eliminar_producto():
         print("Eliminar producto")
+        try:
+            #numero = int(input("Inserte valor del número del producto "))
+            codigo = int(input("Inserte valor numérico del código del producto "))
+            del data_ex[codigo]
+            print("Producto eliminado")
+        except:
+            print("Ocurrió un error, intenta de nuevo") #todo: test para probar errores
 
     print("Gestión de productos, presione tecla para avanzar")
     print("1. Agregar producto")
@@ -87,9 +110,7 @@ def busqueda_de_productos():
     print("Ingrese nombre o código de producto para buscarlo.")
     busqueda = input("Ingresa busqueda: ")
     productos = buscar_producto(data_ex, busqueda)
-    #print(productos) # TODO : mostrar mejor la lista
     print ("A continuación se muestra el listado de productos")
-    print("| # | Cod. | Nombre | Precio | Q | Min. | ") #todo: no en duro
     diccionario_a_tabla(productos)
     opcion = input("Presiona Enter para regresar al menú principal o 1 para buscar otro producto.")
     if opcion == "1":
@@ -99,21 +120,18 @@ def diccionario_a_tabla(lista):
     filas = lista[0:]
 
     def procesar_valor(valor):
-        #print(type(valor))
         if isinstance(valor, dict):
             return " | ".join(map(str, valor.values()))
         return str(valor)
 
-    # Imprimir la tabla
-    #print("|" + "|".join(["-" * len(e) for e in encabezados]) + "|")
-
+    print("| " + " | ".join(map(str, cabecero)) +" |")
     for fila in filas:
         print("| " + " | ".join(map(procesar_valor, fila)) + " |")
 
 def buscar_producto(diccionario, valor):
     resultados = []
     for id_registro, subdiccionario in diccionario.items():
-        if str(valor) == str(subdiccionario['codigo']) or str(valor) in str(subdiccionario['nombre']):
+        if str(valor) == str(id_registro) or str(valor) in str(subdiccionario['nombre']):
             resultados.append((id_registro, subdiccionario))
     return resultados
 
